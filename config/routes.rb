2987ccefcf9512035441c1443542b9e7270a96e7
eugeny naumov <min.verkligheten@gmail.com>
2015-07-15 18:06:15 +0300

    Created a form to capture details for the order and linked to a order model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
-Rails.application.routes.draw do
+Depot::Application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
 
-  resources :products
-  # The priority is based upon order of creation: first created -> highest priority.
+  get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
+  # ...
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -44,7 +49,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
