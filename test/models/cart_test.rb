b7b8ca534883be2488aba65420b908924225e1e4
eugeny naumov <min.verkligheten@gmail.com>
2015-08-04 16:52:57 +0300

    Add cart tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -6,11 +6,27 @@ class CartTest < ActiveSupport::TestCase
   test "quantity is increment when select product twice" do
     cart = Cart.create
 
-    cart.add_product( products(:ruby).id ).save!
-    assert_equal(1, cart.line_items.count)
+    cart.add_product( products(:ruby).id ).save
+    assert_equal 1, cart.line_items.count
 
-    cart.add_product( products(:ruby).id ).save!
-    assert_equal(1, cart.line_items.count)
-    assert_equal(2, cart.line_items.first.quantity)
+    cart.add_product( products(:ruby).id ).save
+    assert_equal 1, cart.line_items.count
+    assert_equal 2, cart.line_items.first.quantity
+  end
+
+  test 'cart should update an existing line item when adding an existing product' do
+    cart = Cart.create
+    cart.add_product(products(:ruby).id).save
+
+    cart.add_product(products(:ruby).id).save
+    assert_equal 1, cart.line_items.count
+  end
+
+  test "cart should be created and destroyed" do
+    assert_same Cart.count, Cart.destroy_all.count,
+      "unable to destroy existing carts"
+    Cart.create
+    assert_equal 1, Cart.count, "unable to create cart"
+    assert_equal 1, Cart.destroy_all.count, "unable to destroy cart"
   end
 end
