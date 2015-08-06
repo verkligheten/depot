8e206e8cdfccbad3b2d5227470e28b949f9c4676
eugeny naumov <min.verkligheten@gmail.com>
2015-08-06 18:53:41 +0300

    Add line_item depend/destroy test

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -9,4 +9,12 @@ class LineItemTest < ActiveSupport::TestCase
     line_item.quantity = 2
     assert_equal 99, line_item.total_price
   end
+
+  test "line_item depend destroy" do
+    cart = Cart.create.add_product(products(:ruby).id)
+    cart.save!
+    assert cart.persisted?
+    cart.destroy
+    assert_nil LineItem.find_by(cart_id: cart.cart_id)
+  end
 end
