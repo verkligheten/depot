8f6cfd7fa6daf873b58dc194d3af8c25701b1a6c
eugeny naumov <min.verkligheten@gmail.com>
2015-08-06 16:47:03 +0300

    Add lite items tests

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "line items should count total price" do
+    line_item = LineItem.create(product_id: products(:ruby).id)
+    assert_equal 49.50, line_item.product.price.to_f
+    line_item.quantity = 2
+    assert_equal 99, line_item.total_price
+  end
 end
