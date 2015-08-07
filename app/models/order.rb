236332059441cc385a664f78ddff607fd3794e50
eugeny naumov <min.verkligheten@gmail.com>
2015-08-07 03:38:05 +0300

    Add regular validation for email

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,16 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
   has_many :line_items, dependent: :destroy
   # ...
-  validates :name, :address, :email, presence: true
+  validates :name, :address, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
+
+  EMAIL_VALID_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: EMAIL_VALID_REGEX }
+
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
